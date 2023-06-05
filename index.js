319df5f791ac010fb20b46410223853e1b90d6cb
Kris Hvattum <klhvattum@live.com>
2023-06-04 21:11:16 -0700

    Write function to list fibonacci using recursive method

--- index.js
+++ index.js
@@ -21,3 +21,27 @@ function fibs(num) {
   }
   
   console.log(fibs(7)); //returns [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+  const fibsRec = (num, arr = [0, 1]) => {
+    if (num < 0) {
+        return 'YOu must enter a whole number'
+    }
+    if (num === 0) {
+        return 'You must enter a whole number'
+    }   
+    if (num === 1) {
+        return [0]
+    }
+    if (num === 2) {
+        return arr
+    }
+
+    if (arr.length < num) {
+        arr.push(arr[arr.length-1] + arr[arr.length-2])
+        return fibsRec(num, arr)
+    }
+
+    return arr
+  }
+
+  console.table(fibsRec(17)); //returns [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987]
